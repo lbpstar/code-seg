a32d5a7fca9b17831a17eafbb1c36f7706c0a346
lbpstar <lbpstar@gmail.com>
2018-10-22 16:23:12 +0800

    20181022

---
+++ AttendanceNG/AttendanceNG/XtraForm1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace AttendanceNG
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.layoutConverter1 = new DevExpress.XtraLayout.Converter.LayoutConverter(this.components);
+            this.simpleButtonChose = new DevExpress.XtraEditors.SimpleButton();
+            this.XtraForm1layoutControl1ConvertedLayout = new DevExpress.XtraLayout.LayoutControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.comboBoxSheet = new System.Windows.Forms.ComboBox();
+            this.textEditFile = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButtonQuery = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem4 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlItem5 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timeEdit1 = new DevExpress.XtraEditors.TimeEdit();
+            this.layoutControlItem6 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.timeEdit2 = new DevExpress.XtraEditors.TimeEdit();
+            this.layoutControlItem7 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            ((System.ComponentModel.ISupportInitialize)(this.XtraForm1layoutControl1ConvertedLayout)).BeginInit();
+            this.XtraForm1layoutControl1ConvertedLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditFile.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButtonChose
+            // 
+            this.simpleButtonChose.Location = new System.Drawing.Point(12, 12);
+            this.simpleButtonChose.Name = "simpleButtonChose";
+            this.simpleButtonChose.Size = new System.Drawing.Size(85, 22);
+            this.simpleButtonChose.StyleController = this.XtraForm1layoutControl1ConvertedLayout;
+            this.simpleButtonChose.TabIndex = 0;
+            this.simpleButtonChose.Text = "选择文件";
+            this.simpleButtonChose.Click += new System.EventHandler(this.simpleButtonChose_Click);
+            // 
+            // XtraForm1layoutControl1ConvertedLayout
+            // 
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.timeEdit2);
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.timeEdit1);
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.textEdit1);
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.comboBoxSheet);
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.textEditFile);
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.simpleButtonChose);
+            this.XtraForm1layoutControl1ConvertedLayout.Controls.Add(this.simpleButtonQuery);
+            this.XtraForm1layoutControl1ConvertedLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.XtraForm1layoutControl1ConvertedLayout.Location = new System.Drawing.Point(0, 0);
+            this.XtraForm1layoutControl1ConvertedLayout.Name = "XtraForm1layoutControl1ConvertedLayout";
+            this.XtraForm1layoutControl1ConvertedLayout.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(1143, 323, 450, 400);
+            this.XtraForm1layoutControl1ConvertedLayout.Root = this.layoutControlGroup1;
+            this.XtraForm1layoutControl1ConvertedLayout.Size = new System.Drawing.Size(404, 219);
+            this.XtraForm1layoutControl1ConvertedLayout.TabIndex = 2;
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(98, 146);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(294, 20);
+            this.textEdit1.StyleController = this.XtraForm1layoutControl1ConvertedLayout;
+            this.textEdit1.TabIndex = 6;
+            // 
+            // comboBoxSheet
+            // 
+            this.comboBoxSheet.FormattingEnabled = true;
+            this.comboBoxSheet.Location = new System.Drawing.Point(187, 36);
+            this.comboBoxSheet.Name = "comboBoxSheet";
+            this.comboBoxSheet.Size = new System.Drawing.Size(205, 22);
+            this.comboBoxSheet.TabIndex = 4;
+            // 
+            // textEditFile
+            // 
+            this.textEditFile.Location = new System.Drawing.Point(187, 12);
+            this.textEditFile.Name = "textEditFile";
+            this.textEditFile.Size = new System.Drawing.Size(205, 20);
+            this.textEditFile.StyleController = this.XtraForm1layoutControl1ConvertedLayout;
+            this.textEditFile.TabIndex = 1;
+            // 
+            // simpleButtonQuery
+            // 
+            this.simpleButtonQuery.Location = new System.Drawing.Point(12, 170);
+            this.simpleButtonQuery.Name = "simpleButtonQuery";
+            this.simpleButtonQuery.Size = new System.Drawing.Size(380, 22);
+            this.simpleButtonQuery.StyleController = this.XtraForm1layoutControl1ConvertedLayout;
+            this.simpleButtonQuery.TabIndex = 5;
+            this.simpleButtonQuery.Text = "开始排查";
+            this.simpleButtonQuery.Click += new System.EventHandler(this.simpleButtonQuery_Click);
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem3,
+            this.layoutControlItem1,
+            this.layoutControlItem4,
+            this.layoutControlItem2,
+            this.layoutControlItem5,
+            this.layoutControlGroup2});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(404, 219);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.comboBoxSheet;
+            this.layoutControlItem3.Location = new System.Drawing.Point(89, 24);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(295, 25);
+            this.layoutControlItem3.Text = " 选择sheet表：";
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(83, 14);
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.textEditFile;
+            this.layoutControlItem1.Location = new System.Drawing.Point(89, 0);
+            this.layoutControlItem1.Name = "textEdit1item";
+            this.layoutControlItem1.Size = new System.Drawing.Size(295, 24);
+            this.layoutControlItem1.Text = "Excel 路径：";
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(83, 14);
+            // 
+            // layoutControlItem4
+            // 
+            this.layoutControlItem4.Control = this.simpleButtonQuery;
+            this.layoutControlItem4.Location = new System.Drawing.Point(0, 158);
+            this.layoutControlItem4.Name = "layoutControlItem4";
+            this.layoutControlItem4.Size = new System.Drawing.Size(384, 41);
+            this.layoutControlItem4.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem4.TextVisible = false;
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.simpleButtonChose;
+            this.layoutControlItem2.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem2.Name = "simpleButton1item";
+            this.layoutControlItem2.Size = new System.Drawing.Size(89, 49);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // layoutControlItem5
+            // 
+            this.layoutControlItem5.Control = this.textEdit1;
+            this.layoutControlItem5.Location = new System.Drawing.Point(0, 134);
+            this.layoutControlItem5.Name = "layoutControlItem5";
+            this.layoutControlItem5.Size = new System.Drawing.Size(384, 24);
+            this.layoutControlItem5.Text = "间隔分钟数：";
+            this.layoutControlItem5.TextSize = new System.Drawing.Size(83, 14);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timeEdit1
+            // 
+            this.timeEdit1.EditValue = new System.DateTime(2018, 9, 7, 0, 0, 0, 0);
+            this.timeEdit1.Location = new System.Drawing.Point(110, 86);
+            this.timeEdit1.Name = "timeEdit1";
+            this.timeEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.timeEdit1.Size = new System.Drawing.Size(270, 20);
+            this.timeEdit1.StyleController = this.XtraForm1layoutControl1ConvertedLayout;
+            this.timeEdit1.TabIndex = 7;
+            // 
+            // layoutControlItem6
+            // 
+            this.layoutControlItem6.Control = this.timeEdit1;
+            this.layoutControlItem6.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem6.Name = "layoutControlItem6";
+            this.layoutControlItem6.Size = new System.Drawing.Size(360, 24);
+            this.layoutControlItem6.Text = "开始:";
+            this.layoutControlItem6.TextSize = new System.Drawing.Size(83, 14);
+            // 
+            // timeEdit2
+            // 
+            this.timeEdit2.EditValue = new System.DateTime(2018, 9, 7, 0, 0, 0, 0);
+            this.timeEdit2.Location = new System.Drawing.Point(110, 110);
+            this.timeEdit2.Name = "timeEdit2";
+            this.timeEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.timeEdit2.Size = new System.Drawing.Size(270, 20);
+            this.timeEdit2.StyleController = this.XtraForm1layoutControl1ConvertedLayout;
+            this.timeEdit2.TabIndex = 8;
+            // 
+            // layoutControlItem7
+            // 
+            this.layoutControlItem7.Control = this.timeEdit2;
+            this.layoutControlItem7.Location = new System.Drawing.Point(0, 24);
+            this.layoutControlItem7.Name = "layoutControlItem7";
+            this.layoutControlItem7.Size = new System.Drawing.Size(360, 24);
+            this.layoutControlItem7.Text = "结束：";
+            this.layoutControlItem7.TextSize = new System.Drawing.Size(83, 14);
+            // 
+            // layoutControlGroup2
+            // 
+            this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem6,
+            this.layoutControlItem7});
+            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 49);
+            this.layoutControlGroup2.Name = "layoutControlGroup2";
+            this.layoutControlGroup2.Size = new System.Drawing.Size(384, 85);
+            this.layoutControlGroup2.Text = " 排除时间范围";
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 219);
+            this.Controls.Add(this.XtraForm1layoutControl1ConvertedLayout);
+            this.Name = "XtraForm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "门禁数据排查";
+            this.Load += new System.EventHandler(this.XtraForm1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.XtraForm1layoutControl1ConvertedLayout)).EndInit();
+            this.XtraForm1layoutControl1ConvertedLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditFile.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.timeEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.Converter.LayoutConverter layoutConverter1;
+        private DevExpress.XtraEditors.SimpleButton simpleButtonChose;
+        private DevExpress.XtraEditors.TextEdit textEditFile;
+        private DevExpress.XtraLayout.LayoutControl XtraForm1layoutControl1ConvertedLayout;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private System.Windows.Forms.ComboBox comboBoxSheet;
+        private DevExpress.XtraEditors.SimpleButton simpleButtonQuery;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem5;
+        private DevExpress.XtraEditors.TimeEdit timeEdit1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem6;
+        private DevExpress.XtraEditors.TimeEdit timeEdit2;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem7;
+    }
+}
---
+++ AttendanceNG/AttendanceNG/XtraForm1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Data.OleDb;
+
+namespace AttendanceNG
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public delegate bool MethodCaller(string file, string sheet);
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButtonChose_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Excel文件(*.xls)|*.xls|Excel文件(*.xlsx)|*.xlsx";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textEditFile.Text = openFileDialog1.FileName.ToString();
+            }
+
+            if (textEditFile.Text.ToString() != "")
+            {
+                comboBoxSheet.Items.Clear();
+                GetExcelSheetName(textEditFile.Text.ToString());
+            }
+        }
+
+        private void simpleButtonQuery_Click(object sender, EventArgs e)
+        {
+            simpleButtonQuery.Enabled = false;
+            simpleButtonQuery.Text = "排查中。。。";
+            string file = textEditFile.Text.ToString().Trim();
+            string sheet = comboBoxSheet.Text.ToString();
+            if (file == "")
+            {
+                MessageBox.Show("没有选择文件", "提示信息", MessageBoxButtons.OK);
+                simpleButtonQuery.Enabled = true;
+
+            }
+            else if (sheet == "")
+            {
+                MessageBox.Show("请选择sheet表", "提示信息", MessageBoxButtons.OK);
+                simpleButtonQuery.Enabled = true;
+            }
+            else
+            {
+                Import(file, sheet);
+                simpleButtonQuery.Enabled = true;
+                simpleButtonQuery.Text = "排查";
+            }
+        }
+        private bool Import(string file, string sheet)
+        {
+            bool success = true;
+            try
+            {
+                DataTable dt;
+                dt = GetDataFromExcel(file, sheet);
+                CheckData(dt);
+            }
+            catch
+            {
+                MessageBox.Show("失败！");
+                success = false;
+            }
+            return success;
+        }
+        /// <summary>
+        /// 从System.Data.DataTable导入数据到数据库
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        private  void CheckData(DataTable dt)
+        {
+            int count = dt.Rows.Count;
+            string fdate = "";
+            string ftype;
+            DateTime _out, _in;
+            TimeSpan ts1,ts2,ts3;
+            _out = DateTime.Now;
+            DataTable dt2 = new DataTable();
+            dt2.Columns.Add("cardno", typeof(string));
+            dt2.Columns.Add("name", typeof(string));
+            dt2.Columns.Add("out", typeof(DateTime));
+            dt2.Columns.Add("in", typeof(DateTime));
+            dt2.Columns.Add("munites", typeof(Decimal));
+            for (int i = 0;i<count;i++)
+            {
+                if (dt.Rows[i]["time"].ToString().Trim() != "" && dt.Rows[i]["type"].ToString().Trim() == "" && dt.Rows[i]["cardno"].ToString().Trim() == "")
+                {
+                    fdate = Convert.ToDateTime(dt.Rows[i]["time"].ToString().Trim()).ToShortDateString().ToString();
+                }
+                else
+                {
+                    dt.Rows[i]["time"] = Convert.ToDateTime(fdate + " " + Convert.ToDateTime(dt.Rows[i]["time"].ToString().Trim()).ToLongTimeString().ToString()); 
+                }
+            }
+            var query_cardno = from row in dt.AsEnumerable()
+                        group row by row.Field<string>("cardno") into m
+                        select new { cardno = m.Key};
+            var query_date = from row in dt.AsEnumerable()
+                               group row by row.Field<DateTime>("time").Date into m
+                               select new { time = m.Key };
+            foreach (var item in query_date)
+            {
+                foreach (var item2 in query_cardno)
+                {
+                    bool tag = false;
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (item.time != null && item2.cardno != null && Convert.ToDateTime(dt.Rows[i]["time"].ToString().Trim()).Date == item.time && dt.Rows[i]["cardno"].ToString().Trim() == item2.cardno)
+                        {
+                            ftype = dt.Rows[i]["type"].ToString().Trim();
+                            if (ftype.Substring(ftype.Length - 1, 1)=="出")
+                            {
+                                _out = Convert.ToDateTime(dt.Rows[i]["time"].ToString().Trim());
+                                tag= true;
+                            }
+                            if (ftype.Substring(ftype.Length - 1, 1) == "入")
+                            {
+                                _in = Convert.ToDateTime(dt.Rows[i]["time"].ToString().Trim());
+                                if(tag == true)
+                                {
+                                    if(Convert.ToDateTime(_in.ToLongTimeString())<= Convert.ToDateTime(timeEdit1.EditValue) || Convert.ToDateTime(_out.ToLongTimeString()) >= Convert.ToDateTime(timeEdit2.EditValue))
+                                    {
+                                        ts1 = _in - _out;
+                                    }
+                                    else
+                                    {
+                                        ts2 = Convert.ToDateTime(timeEdit1.EditValue) - Convert.ToDateTime(_out.ToLongTimeString());
+                                        ts3 = Convert.ToDateTime(_in.ToLongTimeString()) - Convert.ToDateTime(timeEdit2.EditValue);
+                                        if (ts2.Minutes<0)
+                                        {
+                                            ts2 = TimeSpan.Zero;
+                                        }
+                                        if (ts3.Minutes < 0)
+                                        {
+                                            ts3 = TimeSpan.Zero;
+                                        }
+                                        ts1 = ts2 + ts3;
+                                    }
+                                    if (ts1.TotalMinutes>= Convert.ToInt32(textEdit1.Text))
+                                    {
+                                        dt2.Rows.Add(item2.cardno, dt.Rows[i]["name"].ToString().Trim(), _out,_in, Math.Ceiling(ts1.TotalMinutes));
+                                    }
+                                }
+                                tag = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if(dt2.Rows.Count ==0)
+            {
+                MessageBox.Show("没有发现异常！");
+            }
+            else
+            {
+                Common.DataSetToExcel(dt2, true);
+            }
+           
+        }
+
+        private void GetExcelSheetName(String fileName)
+        {
+            try
+            {
+                string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\";", fileName);
+                OleDbConnection conn = new OleDbConnection(connectionString);
+                string name;
+                conn.Open();
+                DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                for (int i = 0; i < schemaTable.Rows.Count; i++)
+                {
+                    name = schemaTable.Rows[i]["TABLE_NAME"].ToString();
+                    comboBoxSheet.Items.Insert(i, name);
+                    //comboBox2.DataSource = comboBox2.Items;
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        /// <summary>
+        /// 获取指定 Excel 文件中工作表的数据
+        /// </summary>
+        /// <param name="fileName">Excel 的文件名</param>
+        /// <returns></returns>
+        private DataTable GetDataFromExcel(String fileName, String sheet)
+        {
+            if (!String.IsNullOrEmpty(sheet))
+            {
+                string commandText = String.Format("SELECT  time,event,type,cardno,name FROM [{0}] where ltrim(rtrim(time)) <> '' and time is not null", sheet);
+                return this.ExecuteDataTable(fileName, commandText);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        /// <summary>
+        /// 获取指定 Excel 文件中工作表的数据。
+        /// </summary>
+        /// <param name="fileName">Excel 的文件名</param>
+        /// <param name="commandText">查询 SQL </param>
+        private DataTable ExecuteDataTable(String fileName, String commandText)
+        {
+            string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\";", fileName);
+            using (OleDbDataAdapter da = new OleDbDataAdapter(commandText, connectionString))
+            {
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return ds.Tables[0];
+            }
+        }
+
+
+        private void XtraForm1_Load_1(object sender, EventArgs e)
+        {
+            textEdit1.Text = "30";
+            timeEdit1.EditValue = "11:30";
+            timeEdit2.EditValue = "14:00";
+        }
+    }
+}
